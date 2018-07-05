e30e208271a351406d3f23281f1ddcb4b4665572
je-ni <jenniferolibie@gmail.com>
2018-07-05 12:40:09 +0100

    enabled adding and getting

---
+++ Controllers/CategoryController.js
@@ -0,0 +1,34 @@
+var service = require('../Services/CategoryService');
+
+exports.addCategory = function(req, res){
+    var data = {
+        category: req.body.category   
+    };
+    return service.addCategory(req, res, data);
+}
+
+exports.getCategories = function(req, res){
+    return service.getAllCategories(req, res);
+}
+
+// exports.deleteUser = function(req, res){
+//     var option = {_id: req.params.id};
+//     return service.deleteUser(req, res, option);
+// }
+
+// exports.getUserByParam = function(req, res){
+//     var option = req.query;
+//     return service.getUsersByParam(req, res, option);
+// }
+
+// exports.updateUser = function(req, res){
+//     var id = req.params.id;
+//     var update = req.body;
+//     return service.updateUser(req, res, id, update);    
+// }
+
+// exports.addFriend = function(req, res){
+//     var user_id = req.query.user_id;
+//     var friend_id = req.query.friend_id;
+//     return service.addUserAsFriend(res, res, user_id, friend_id);
+// }
