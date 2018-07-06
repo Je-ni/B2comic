b30f6bf4dc7fe7dfe5709741c1c32a31edb98ca9
je-ni <jenniferolibie@gmail.com>
2018-07-06 10:32:22 +0100

    added comic routers

---
+++ routes/comic.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var comicController = require('../Controllers/ComicController');
+
+/* GET comics listing. */
+router.get('/', comicController.getComics);
+
+router.post('/add', comicController.addComic);
+
+router.get('/delete', comicController.deleteComic);
+
+router.get('/search', comicController.getComicByParam);
+
+module.exports = router;
